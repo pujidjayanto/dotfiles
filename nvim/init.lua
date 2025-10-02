a085e0f6be18f299c210fa4f9ec03d9431a1469f
Dedy Puji <dedypuji@outlook.com>
2025-10-02 10:50:41 +0700

    Update init.lua
    - add lsp and mason
    - adjust the code to be more readable

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,134 +1,146 @@
--- vim tab setup
--- set tab size to 2 spaces
+-----------------------------------------------------------
+-- BASIC SETTINGS
+-----------------------------------------------------------
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- this is for yank and paste to another apps
-vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
+vim.opt.clipboard = "unnamedplus" -- system clipboard
+vim.opt.nu = true                 -- show line numbers
 
--- this is for line numbers, i hate relative line number
-vim.opt.nu = true                 -- set line numbers
-
+vim.g.mapleader = " "             -- leader key = space
 
 
-
--- Bootstrap lazy.nvim --------------------------
--- https://lazy.folke.io/installation
+-----------------------------------------------------------
+-- BOOTSTRAP lazy.nvim
+-----------------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
 
--- Setup lazy.nvim
+-----------------------------------------------------------
+-- PLUGINS
+-----------------------------------------------------------
 require("lazy").setup({
   spec = {
-        {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-        {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = { "nvim-tree/nvim-web-devicons" }, -- for file icons
-    },
-        {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
--- Bootstrap lazy.nvim -----------------------------
+    -- UI
+    { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 
+    -- LSP + Completion
+    -- mason will install the lsp instead of manually install
+    -- a portable package manager used to easily install and manage editor tools like Language Server Protocol (LSP) servers, Debug Adapter Protocol (DAP) servers, linters, and formatters
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim", config = true },
+    { "williamboman/mason-lspconfig.nvim" },
 
-
--- setup plugins ----
--- lua line
-require("lualine").setup()
--- configure nvim-tree
-require("nvim-tree").setup({
-  view = {
-    width = 50,
-    side = "right",
-  },
-  renderer = {
-    icons = {
-      show = {
-        git = true,
-        file = true,
-        folder = true,
+    -- Autocompletion engine
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
       },
     },
   },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
+  checker = { enabled = true },
 })
--- toggle nvim-tree like VSCode explorer
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>e",
-  ":NvimTreeToggle<CR>",
-  { noremap = true, silent = true }
-)
 
--- move focus from file to tree again
-vim.keymap.set("n", "<leader>n", function()
-  require("nvim-tree.api").tree.focus()
-end, { desc = "Focus nvim-tree" })
 
--- configure nvim-tree
---
--- setup telescope with fzf
--- https://github.com/nvim-telescope/telescope-fzf-native.nvim?tab=readme-ov-file#telescope-setup-and-configuration
--- You dont need to set any of these options. These are the default ones. Only
--- the loading is important
-require('telescope').setup {
+-----------------------------------------------------------
+-- UI PLUGINS CONFIG
+-----------------------------------------------------------
+-- Lualine
+require("lualine").setup()
+
+-- Nvim-tree
+require("nvim-tree").setup({
+  view = { width = 50, side = "right" },
+  update_focused_file = { enable = true, update_cwd = true },
+})
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle Explorer" })
+vim.keymap.set("n", "<leader>n", function() require("nvim-tree.api").tree.focus() end, { desc = "Focus Explorer" })
+
+-- Telescope
+require("telescope").setup({
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
     }
   }
-}
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
--- setup telescope with fzf
+})
+require("telescope").load_extension("fzf")
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search text" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
 
 
---- setup plugins ----
+-----------------------------------------------------------
+-- AUTOCOMPLETION (nvim-cmp)
+-----------------------------------------------------------
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(), -- trigger completion
+    ["<C-e>"] = cmp.mapping.abort(),        -- close completion
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- accept completion
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll docs up
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),  -- scroll docs down
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
 
 
+-----------------------------------------------------------
+-- LSP (Mason + TS/JS/TSX)
+-----------------------------------------------------------
+require("mason").setup()
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup({
+  ensure_installed = { "ts_ls" }, -- Mason server name
+  automatic_installation = true,
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- Use ts_ls to avoid deprecation warning
+vim.lsp.enable("ts_ls", {
+  capabilities = capabilities,
+})
+
+
+-- LSP keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
